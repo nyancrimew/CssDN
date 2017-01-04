e4f8fc42715e0be73e918609ae1e16ace2250f9e
deletescape <deletescape@outlook.com>
2017-01-04 11:13:57 +0100

    Only pull master branches

--- build.sh
+++ build.sh
@@ -13,10 +13,8 @@ mkdir build/
 echo
 echo Cloning github-syntax-theme-generator
 echo
-git clone --depth=1 https://github.com/primer/github-syntax-theme-generator.git repos/github-syntax-theme-generator
-
+git clone --depth=1 --branch=master https://github.com/primer/github-syntax-theme-generator.git repos/github-syntax-theme-generator && cd repos/github-syntax-theme-generator
 echo
-cd repos/github-syntax-theme-generator
 echo Loading dependencies of github-syntax-theme-generator
 echo
 npm install
@@ -32,7 +30,7 @@ cd ../..
 echo
 echo Cloning normalize.css
 echo
-git clone --depth=1 https://github.com/necolas/normalize.css.git repos/normalize && cd repos/normalize
+git clone --depth=1 --branch=master https://github.com/necolas/normalize.css.git repos/normalize && cd repos/normalize
 echo
 echo Copying artifacts to output directory
 cp normalize.css ../../build
